35b069f204bd5185ab901a168adf94a45b5fb243
David Jackson <70109697+Murazor-byte@users.noreply.github.com>
2023-05-06 17:12:37 -0500

    Update pathfindingGrid.cs

--- Scripts/Combat/Pathfinding/pathfindingGrid.cs
+++ Scripts/Combat/Pathfinding/pathfindingGrid.cs
@@ -1,44 +1,99 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 /*
- * Code inspired from CodeMonkey
+ * Code Inspired from CodeMonkey
 */
 
-public class PathNode
+public class PathfindingGrid
 {
-    private PathfindingGrid pathfindingGrid;
+    private int width;
+    private int hieght;
 
-    public int x;
-    public int z;
+    public PathNode[,] pathfindingGrid;
 
-    public int gCost;                   //cost from start node
-    public int hCost;                   //cost of direct path from startNode to endNode
-    public int fCost;                   //gCost + hCost
-    public int penatlyCost = 0;         //penatly for this node (effect tile)
-    public int proximityPenalty = 0;    //cumaltive penalty for proximity to target (only for "Retreat" pathfinding)
-
-    public bool isWalkable;
-
-    public PathNode previousNode;
-
-    public PathNode(PathfindingGrid pathfindingGrid, int x, int z)
+    public PathfindingGrid(int width, int hieght, TileGenerator.TileType[][] tileType, TileGenerator.Traversability[][] traversibility, Func<PathfindingGrid, int, int, PathNode> gridObject)
     {
-        this.pathfindingGrid = pathfindingGrid;
-        this.x = x;
-        this.z = z;
-        isWalkable = true;
+        this.width = width;
+        this.hieght = hieght;
+
+        pathfindingGrid = new PathNode[width, hieght];
+        for (int i = 0; i < pathfindingGrid.GetLength(0); i++)
+        {
+            for (int j = 0; j < pathfindingGrid.GetLength(1); j++)
+            {
+                pathfindingGrid[i,j] = gridObject(this,i,j);
+
+                //sets path nodes to be unwalkable
+                if (traversibility != null && traversibility[i][j] != TileGenerator.Traversability.Walkable)
+                {
+                    pathfindingGrid[i, j].isWalkable = false;
+                }
+
+                //setting the nodes penatly costs
+                switch (tileType[i][j])
+                {
+                    case TileGenerator.TileType.Lava:
+                        pathfindingGrid[i, j].penatlyCost = 50;
+                        break;
+                    case TileGenerator.TileType.Water:
+                        pathfindingGrid[i, j].penatlyCost = 20;
+                        break;
+                    case TileGenerator.TileType.Pit:
+                        pathfindingGrid[i,j].penatlyCost = 50;
+                        break;
+                    default:
+                        pathfindingGrid[i, j].penatlyCost = 0;
+                        break;
+                }
+            }
+        }
     }
 
-    public void CalculateFCost()
+    //when passing as a Vector3 => tuple
+    public void GetGridObject(Vector3 worldPosition, out int x, out int y)
     {
-        fCost = gCost + hCost;
+        x = Mathf.FloorToInt(worldPosition.x);
+        y = Mathf.FloorToInt(worldPosition.y);
     }
 
-    //sets the current nodes walkable trait
-    public void SetIsWalkable(bool isWalkable)
+    //when passing as a tuple
+    public PathNode GetGridObject(int x, int y)
     {
-        this.isWalkable = isWalkable;
+        if (x >= 0 && y >= 00 && x < width && y < hieght)
+        {
+            return pathfindingGrid[x,y];
+        }
+        else
+        {
+            return default(PathNode);
+        }
     }
+
+    //When passing as a tuple
+    public void SetGridValue(int x, int y, PathNode value)
+    {
+        pathfindingGrid[x,y] = value;
+    }
+    
+    //when passing as a Vector3 => tuple
+    public void SetGridValue(Vector3 worldPosition, PathNode value)
+    {
+        int x, y;
+        GetGridObject(worldPosition, out x, out y);
+        SetGridValue(x, y, value);
+    }
+
+    public int GetWidth()
+    {
+        return width;
+    }
+
+    public int GetHeight()
+    {
+        return hieght;
+    }
+    
 }
